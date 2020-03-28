52e1f05c54354d79be2404c3345add5fa601d9f8
priaajisn <priaaji.setyanegoro@gmail.com>
2020-03-28 22:00:23 +0700

    dictonaries as obj on js

---
+++ dictonaries.py
@@ -0,0 +1,12 @@
+# dictonaries same with obj on js
+users = {
+    "name"  : "Ed",
+    "email" : "ed@gmail.com",
+    "age"   : 20
+}
+
+# print(users["name"])
+
+for key,val in users.items():
+    print(key)
+    print(val)
