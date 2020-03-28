4254c1515a12c8bedfb5cc26622910e29c09efbe
priaajisn <priaaji.setyanegoro@gmail.com>
2020-03-28 21:50:02 +0700

    function on py

---
+++ func.py
@@ -0,0 +1,11 @@
+def greetings():
+    print("hello whats is ur name ?")
+    name = input()
+    print("hello " + name)
+
+def multiply_by_2(n):
+    return n*2 
+
+
+greetings()
+print(multiply_by_2(5))
