a484976b9fb13a060efd45b7a032036dfaad427c
priaajisn <priaaji.setyanegoro@gmail.com>
2020-03-28 20:36:47 +0700

    upload files

---
+++ app.py
@@ -0,0 +1,27 @@
+#variabel is dynamic type
+
+name = "Pria Aji sn" #string
+card = 3 #number
+randNum = 4.6 #float
+
+print(name)
+print(card)
+print(randNum)
+
+#concatination
+first_name = "Makaji"
+last_name = "Kenduri"
+greetings = f"Hello there ma name is {first_name} {last_name} and I LOVE MONEY "
+quotes = "makajikenduri says \"if you never try you never ever know\" "
+
+#multiple strings
+sings = """LALLAALLA
+LALALLA
+LALALA
+"""
+
+full_name = first_name + " " + last_name
+print(full_name)
+print(greetings)
+print(quotes)
+print(sings)
---
+++ methods.py
@@ -0,0 +1,14 @@
+gretting = "hello dear my name is aji"
+
+#count of word
+print(len(gretting))
+
+# access index each individual letter
+letter = gretting[0:4]
+print(letter)
+
+# string method
+upper_string = gretting.upper()
+replace_name = gretting.replace('aji', 'makajikenduri')
+print(upper_string)
+print(replace_name)
