296fa01342cd34a38c5c82c24e715baa0d72c9f4
priaajisn <priaaji.setyanegoro@gmail.com>
2020-03-28 20:56:59 +0700

    update conditional and add array

--- conditional.py
+++ conditional.py
@@ -7,3 +7,9 @@ elif age == 60 :
     print("Hey you're pro than us")
 else:
     print("You can join the club")
+
+my_age = 21
+your_age = 50
+
+if my_age > 20 and your_age > 30 :
+    print('both of you\'re old enought')
---
+++ list.py
@@ -0,0 +1,16 @@
+# Array 
+users = ["AJI","REZA","AJAT","IMAM","MAYA"]
+print(users[0])
+
+# otomatic generate array
+alot_zero = [0] * 20
+print(alot_zero)
+
+# Unpacking array 
+items = ['laptops' , 'phone' , 'joystick']
+laptop = items[0]
+print(laptop)
+
+# distratring array 
+laptop,phone,joystick = items
+print(joystick)
