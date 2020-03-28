91334756e1a792e13aeb59b49d4187e9fc4ba578
priaajisn <priaaji.setyanegoro@gmail.com>
2020-03-28 21:37:03 +0700

    looping

---
+++ looping.py
@@ -0,0 +1,18 @@
+users = ["AJI","REZA","AJAT","IMAM","MAYA"]
+
+for user in users:
+    print(user)
+
+# each char of string 
+name = list("Edwin")
+print(name)
+
+# generate each list 
+my_list = list(range(0,101,5))
+print(my_list)
+
+# while
+a = 0 
+while a < 10 : 
+    print(a)
+    a += 1 
